0bfcfe96bb2162b2e5c6d4b80457a47c3ecd1568
DD <68047131+xF346x@users.noreply.github.com>
2020-08-02 13:34:32 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+/* Config ETHDRAIN */
+
+$keys = "HPf8teObZWGkgFyoqNPZpJUQLMKBYWIzXBMMZOINSZQONPVNYY"; // Ambil Disini => https://cb.run/ypcd
+$useragent ="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.3 Safari/605.1.15"; // Cari Di google banyak
+$userdata = "0x2da2eF9b056f343F0dC59158C702e3f585B3Ba0d"; // userdata bisa pakai email Coinbase atau pakai eth address Coinbase 
+
